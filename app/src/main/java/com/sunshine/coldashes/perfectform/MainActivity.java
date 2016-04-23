9c46bc5ba7550b41f9bce2ce99de8bb9b9127157
Ronald Manganaro <ronaldmanganaro@gmail.com>
2016-04-23 11:33:53 -0500

    Added the ability to send the zip code entered by the user to the weather activity

---
+++ app/src/main/java/com/sunshine/coldashes/perfectform/MainActivity.java
@@ -0,0 +1,112 @@
+package com.sunshine.coldashes.perfectform;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by ronald on 4/7/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    private EditText zipcode_edittext;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        zipcode_edittext = (EditText) findViewById(R.id.zipcode_edittext);
+
+
+        getSupportActionBar().setTitle("Running Buddy");
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                //makeUseOfNewLocation(location);
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, location.describeContents(), duration);
+                toast.show();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.appbar, menu);
+        String title = "Running Buddy";
+        int groupID = Menu.NONE;
+        int itemID = Menu.FIRST;
+        int order = 103;
+        menu.add(groupID,itemID,order,title);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+
+                return true;
+            case R.id.action_analyze:
+                Intent myIntent = new Intent(this, SensorAccelerometer.class);
+                startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void changeToWeather (View view) {
+        int theZip = Integer.parseInt(zipcode_edittext.getText().toString());
+        Intent i = new Intent(getApplicationContext(), WeatherActivity.class);
+        i.putExtra("zip",theZip);
+        startActivity(i);
+    }
+}
